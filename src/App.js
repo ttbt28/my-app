3c33da2e1dd107c26161a9a0e57dd6553ea2cce5
Thuy Bao Tran Tran <ttran28@uw.edu>
2023-06-23 14:18:00 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="tel:253-359-4873"> Call Anh Yeu </a>
       </header>
     </div>
   );
